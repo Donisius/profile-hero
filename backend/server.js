fc3bcbfb012240047bf13acbb7e809a4dcc27f94
Donisius Wigie <donisius.wigie@gmail.com>
2020-03-07 10:22:48 -0500

    Add content to server

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.post('/api/upload-text', (req, res) => {
+});
+
+app.get('/', (req, res) => {
+    res.send('')
+})
